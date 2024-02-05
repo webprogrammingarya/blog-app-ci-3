99b1141b5293aa986ed9188e3616e01ddff10607
Arya Dimas <webprogrammingarya>
2024-02-05 11:03:38 +0700

    di view, judulnya dibenerin dikit

--- application/views/blog.php
+++ application/views/blog.php
@@ -7,7 +7,7 @@
                     <div class="col-md-10 col-lg-8 col-xl-7">
                         <div class="site-heading">
                             <h1>Selamat Datang</h1>
-                            <span class="subheading">di DiaryKu</span>
+                            <span class="subheading">di BlogKu</span>
                         </div>
                     </div>
                 </div>
